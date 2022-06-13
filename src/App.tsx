b79bf21cb1e36ea322f0eb0429ffbda63e851400
liuminghui <minghui.liu@palmpay-inc.com>
2022-06-13 14:14:13 +0800

    refactor: youhualuyou

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 // import logo from './logo.svg';
 import '@/App.scss'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import routes from '@/router'
 import '@/until/rem'
 // import { renderRoutes } from 'react-router-config';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 
 const App = () => {
+  const [routeArr] = useState(routes)
+  useEffect(() => {
+    console.log('++++++++++++routes+++++++++++', routeArr)
+  }, [routeArr])
   return (
     <Router>
       <Switch>
-        {routes.map((r, i) => {
+        {routeArr.map((r, i) => {
           return <Route exact={r.exact} path={r.path} key={i} render={() => {
             document.title = r.name || ''
             return <r.component></r.component>
